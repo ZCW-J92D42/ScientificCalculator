bef4749907c79d3535491233babd199b3d81a750
Loc <loc@Zipcoders-MacBook-Pro-2.local>
2023-10-29 19:13:18 -0400

    started making switch cases, basic ones work, complex ones dont

--- src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
+++ src/main/java/com/zipcodewilmington/scientificcalculator/MainApplication.java
@@ -28,12 +28,46 @@ public class MainApplication {
         //ignore below
         DisplayClass.clearDisplay();
         String s = Console.getStringInput("Enter your math problem");
-        Double num1 = DisplayClass.integerExtractor(s)[0];
-        Double num2 = DisplayClass.integerExtractor(s)[1];
+        //Double num1 = DisplayClass.integerExtractor(s)[0];
+        //Double num2 = DisplayClass.integerExtractor(s)[1];
+        Double[] num = DisplayClass.integerExtractor(s);
         String operator = DisplayClass.operatorExtractor(s);
-        System.out.println(num1);
-        System.out.println(num2);
-        System.out.println(operator);
+        //System.out.println(num1);
+        //System.out.println(num2);
+        //System.out.println(operator);
+
+        Calculator calculator = new Calculator();
+        //determines which operators to use
+        switch(operator){
+            case "+":
+                System.out.println(calculator.addNumbers(num[0], num[1]));
+                break;
+            case "-":
+                System.out.println(calculator.subtractNumbers(num[0], num[1]));
+                break;
+            case "*":
+                System.out.println(calculator.multiplyNumbers(num[0], num[1]));
+                break;
+            case "/":
+                if(num[1] == 0.0){
+                    System.out.println("ERR");
+                }else{
+                System.out.println(calculator.divideNumbers(num[0], num[1]));
+                }
+                break;
+            case "sq":
+                System.out.println(calculator.square(num[0]));
+                break;
+            case "sqrt":
+                System.out.println(calculator.squareRoot(num[0]));
+                break;
+            case "inv":
+                System.out.println(calculator.inverse(num[0]));
+                break;
+            case "pow":
+                System.out.println(calculator.exponentiation(num[0], num[1]));
+                break;
+        }
 
     }
 
